9c16722169531a1bd9b5f3c2ab55fc5a367ff896
iticbnc <dlopezsmx2013@gmail.com>
2024-10-08 20:32:18 +0200

    star algorithm

--- ai/star-algorithm/star.py
+++ ai/star-algorithm/star.py
@@ -3,68 +3,83 @@ import time
 from queue import PriorityQueue
 from math import sqrt
 
-ROWS = 20
-COLS = 20
+ROWS = 50
+COLS = 50
+POSSIBLE_PATHS = 0 # 0 => only 1 path | 99 (max) => multiple paths
 
-def distance(cell1, cell2):
+# Euclidean distance
+def distanceEuclidean(cell1, cell2):
     return sqrt((cell2[0] - cell1[0]) ** 2 + (cell2[1] - cell1[1]) ** 2)
 
-def aStar(m, init_position, goal_position):
-    # Save positions with the a* value => positions
-    forward_path = PriorityQueue()
-    position = init_position
-    point = m.maze_map[init_position]
+# Manhattan distance
+def distanceManhattan(cell1, cell2):
+    return abs(cell1[0] - cell2[0]) + abs(cell1[1] - cell2[1])    
+
+def distance(cell1, cell2):
+    return distanceManhattan(cell1, cell2)
+
+def aStar(m, a):
+    start_position = a.position
+    goal_position = a.goal
+
+    g_score = { cell: float('inf') for cell in m.grid }
+    f_score = { cell: float('inf') for cell in m.grid }
+
+    g_score[start_position] = 0
+    f_score[start_position] = distance(start_position, goal_position)
+
+    forward_path = {}
     extended_list = {}
-    aux = 0
 
-    while aux < 5:
-        distance_from_init = 0
-        distance_to_goal = distance(init_position, goal_position)
-        total_distance = distance_from_init + distance_to_goal
-        pq.put(total_distance)
+    open = PriorityQueue()
+    open.put((distance(start_position, goal_position), distance(start_position, goal_position), start_position))
 
-        print(total_distance, point)
-        x, y = position
+    while not open.empty():
+        current_cel = open.get()[2]
 
-        validMovements = sum(point[index] for index in point)
-        newX = x
-        newY = y
+        if current_cel == goal_position:
+            break
 
-        for i in range(validMovements):
-            if point['E']:
-                if (newX, newY + 1) not in extended_list:
-                    newY = y + 1
-                    print('Move to east', newX, newY)
-            elif point['W']:
-                if (newX, newY - 1) not in extended_list:
-                    newY = y - 1
-                    print('Move to west', newX, newY)
-            elif point['N']:
-                if (newX + 1, newY) not in extended_list:
-                    newX = x + 1
-                    print('Move to north', newX, newY)
-            elif point['S']:
-                if (newX - 1, newY) not in extended_list:
-                    newX = x - 1
-                    print('Move to south', newX, newY)
+        for direction in 'ESNW':
+            if m.maze_map[current_cel][direction] != True:
+                continue
 
-        newPosition = (newX, newY)
-        extended_list[newPosition] = True
-        position = newPosition
+            if direction == 'E':
+                child_cel = (current_cel[0], current_cel[1] + 1)
+            if direction == 'W':
+                child_cel = (current_cel[0], current_cel[1] - 1)
+            if direction == 'N':
+                child_cel = (current_cel[0] - 1, current_cel[1])
+            if direction == 'S':
+                child_cel = (current_cel[0] + 1, current_cel[1])
 
-        aux = aux + 1
+            current_g_score = g_score[current_cel] + 1
+            current_f_score = current_g_score + distance(child_cel, goal_position)
+
+            if current_f_score < f_score[child_cel]:
+                g_score[child_cel] = current_g_score
+                f_score[child_cel] = current_f_score
+
+                open.put((current_f_score, distance(child_cel, goal_position), child_cel))
+                extended_list[child_cel] = current_cel
+
+    cell = goal_position
+
+    while cell != start_position:
+        forward_path[extended_list[cell]] = cell
+        cell = extended_list[cell]
 
     return forward_path
 
-m=maze.maze(ROWS,COLS)
-m.CreateMaze()
-a=maze.agent(m,footprints=True)
+m=maze.maze(ROWS, COLS)
+m.CreateMaze(loopPercent=POSSIBLE_PATHS)
+a=maze.agent(m, footprints=True)
 
 pre_Astar = time.time()
-path = aStar(m, a.position, a.goal)
+path = aStar(m, a)
 post_Astar = time.time()
 
-# print(post_Astar - pre_Astar)
+print(post_Astar - pre_Astar)
 
-m.tracePath({a:path},delay=5)
+m.tracePath({ a: path }, delay=5)
 m.run()
